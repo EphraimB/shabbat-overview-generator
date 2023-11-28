5465cb535b7fbddd826d6caeee693af43038e23f
Ephraim Becker <emb16@outlook.com>
2023-11-28 17:14:46 -0500

    Trying to create the perfect sun path.

--- shabbat-overview.py
+++ shabbat-overview.py
@@ -1,10 +1,12 @@
-SHABBAT_START = "16:11"  # Example start time (candle lighting)
+SHABBAT_START = "16:29"  # Example start time (candle lighting)
 SHABBAT_END = "17:13"    # Example end time (Havdalah)
 UNIT_SYSTEM = "imperial"  # or "metric"
 
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime, timedelta
 
 def convert_to_12hr_format(time_str):
     # Assuming the time_str is in 'HH:MM' format
@@ -14,7 +16,28 @@ def convert_to_12hr_format(time_str):
 def celsius_to_fahrenheit(celsius_temp):
     return (celsius_temp * 9/5) + 32
 
-def create_pdf(weather, filename):
+def create_sun_path_image(zmanim, weather, filename):
+    # Create the plot
+    fig, ax = plt.subplots()
+    for zman in zmanim:
+        ax.plot(zmanim[zman], 0, 'ro')  # Plot zmanim times
+        ax.text(zmanim[zman], 0.1, zman, rotation=45)
+
+    # Add weather data (optional, based on your data)
+    # for forecast in weather:
+    #     ax.plot(forecast['time'], forecast['temperature'], 'bo')
+    #     ax.text(forecast['time'], forecast['temperature'] + 0.1, forecast['forecast'], rotation=45)
+
+    # Format the plot
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    # Save the plot as an image
+    plt.savefig(filename)
+    plt.close()
+
+def create_pdf(sun_path_img, filename):
     c = canvas.Canvas(filename, pagesize=letter)
     width, height = letter
 
@@ -47,10 +70,17 @@ def create_pdf(weather, filename):
         if y < 50:  # Check to avoid writing off the page
             break
 
+    # Add the sun path image at the bottom of the PDF
+    c.drawImage(sun_path_img, 50, 50, width=500, height=100)  # Adjust size and position as needed
+
     c.save()
 
-# Mock Weather Data spanning 25 hours of Shabbat
-mock_weather = [
+zmanim = {
+    "Candle Lighting": datetime(2023, 4, 14, 18, 0),
+    "Havdalah": datetime(2023, 4, 15, 19, 0)
+}
+# Weather Data spanning 25 hours of Shabbat
+weather = [
     {"day": "Friday", "time": "18:00", "forecast": "Clear", "temperature": 15},
     {"day": "Friday", "time": "19:00", "forecast": "Partly Cloudy", "temperature": 14},
     # ... more data for Friday evening
@@ -61,4 +91,8 @@ mock_weather = [
 ]
 
 
-create_pdf(mock_weather, "Shabbat_Overview.pdf")
+sun_path_filename = "sun_path.png"
+create_sun_path_image(zmanim, weather, sun_path_filename)
+
+pdf_filename = "Shabbat_Overview.pdf"
+create_pdf(sun_path_filename, pdf_filename)
